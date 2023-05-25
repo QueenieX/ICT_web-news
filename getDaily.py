e7d2141ab82b7e6c7504ca0a57d858252759a079
Queenie <112835921+QueenieX@users.noreply.github.com>
2023-05-25 11:47:30 +0930

    Update and rename getDailyUrls.py to getDaily.py

--- getDaily.py
+++ getDaily.py
@@ -14,7 +14,7 @@ for rss_url in rss_urls:
     for entry in feed.entries:
         print(entry.link)
         
-        if entry.link.startswith("https://www.news.com.au/content-feeds/latest-news-national/"):
+        if entry.link.startswith("https://www.news.com.au/content-feeds/latest-news-national/" or "https://www.news.com.au/content-feeds/latest-news-world/"):
             response = requests.get(entry.link)
             soup = BeautifulSoup(response.text, "html.parser")
             articles = soup.find_all('div', class_='story-article')
@@ -24,23 +24,29 @@ for rss_url in rss_urls:
                 
                 date_str = article.find('div', class_='byline_publish').text.strip().split(' - ')[0]
                 published_date = datetime.datetime.strptime(date_str, "%B %d, %Y")
-        
-        elif entry.link.startswith("https://www.news.com.au/content-feeds/latest-news-world/"):
-            response = requests.get(entry.link)
-            soup = BeautifulSoup(response.text, "html.parser")
-            articles = soup.find_all('div', class_='story-article')
-            for article in articles:
-                link = article.find('a')['href']
-                title = article.find('a').text.strip()
+
+                article_content = soup.find('div', id='story-primary')
+                content_text = article_content.text.strip()
                 
-                date_str = article.find('div', class_='byline_publish').text.strip().split(' - ')[0]
-                published_date = datetime.datetime.strptime(date_str, "%B %d, %Y")
         else:
             # For 9news.com.au, use the pubDate attribute
             if "published" in entry:
                 published_date = datetime.datetime.strptime(
                     entry.published, "%a, %d %b %Y %H:%M:%S %z"
                 ).timetuple()
+                response = requests.get(entry.link)
+
+                # Create a BeautifulSoup object from the response content
+                soup = BeautifulSoup(response.content, 'html.parser')
+                title = soup.find('h1').text.strip()
+                article_contents = soup.find_all('div', class_='block-content')
+
+                # Extract the text content from all the span elements in each block-content div
+                content = ""
+                for content_div in article_contents:
+                    content_spans = content_div.find_all('span')
+                    for span in content_spans:
+                        content += span.text.strip() + " "
             else:
                 continue
             
